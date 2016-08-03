2ce1f261663ec3eb1d287adc21eabd36b12c5cc7
Sean Gallagher <seanpg94@gmail.com>
2016-08-03 10:15:39 -0500

    added permissions request for location

--- Sample/app/src/main/java/com/phunware/core/sample/CoreDemo.java
+++ Sample/app/src/main/java/com/phunware/core/sample/CoreDemo.java
@@ -1,15 +1,22 @@
 package com.phunware.core.sample;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.phunware.core.PwCoreSession;
 
 public class CoreDemo extends Activity {
 
+    private static final int RC_PERM = 2330;
+    private boolean permissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,17 @@ public class CoreDemo extends Activity {
                 startActivity(i);
             }
         });
+
+        checkPermissions();
     }
     @Override
     protected void onStart() {
         super.onStart();
+
+        if (!permissionGranted) {
+            return;
+        }
+
 		/*
 		 * Start Core Session
 		 */
@@ -36,9 +50,56 @@ public class CoreDemo extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+
+        if (!permissionGranted) {
+            return;
+        }
+
 		/*
 		 * Stop Core Session
 		 */
         PwCoreSession.getInstance().activityStopSession(this);
+    }
+
+    private void checkPermissions() {
+        if (!canAccessLocation()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, RC_PERM);
+            }
+        } else {
+            onLocationPermissionGranted();
+        }
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return(PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RC_PERM) {
+            if (canAccessLocation()) {
+                onLocationPermissionGranted();
+            } else {
+                Toast.makeText(CoreDemo.this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void onLocationPermissionGranted() {
+        permissionGranted = true;
+        PwCoreSession.getInstance().registerKeys(this,
+                getString(R.string.app_appid),
+                getString(R.string.app_accesskey),
+                getString(R.string.app_signaturekey),
+                getString(R.string.app_encryptionkey));
+
     }
 }
