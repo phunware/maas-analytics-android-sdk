3231d835ba695b1da88e33b9079f45347948f6b2
Sean Gallagher <seanpg94@gmail.com>
2017-06-29 23:17:38 -0500

    updates to sample app for 3.1.0 (#12)

--- Sample/app/src/main/java/com/phunware/core/sample/SecondActivity.java
+++ Sample/app/src/main/java/com/phunware/core/sample/SecondActivity.java
@@ -1,8 +1,6 @@
 package com.phunware.core.sample;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
