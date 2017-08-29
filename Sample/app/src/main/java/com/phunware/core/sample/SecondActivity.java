b9c738946ed33f1a2debd24cd51b68db0800980c
Sean Gallagher <seanpg94@gmail.com>
2017-08-29 16:37:47 -0500

    updates for 3.1.1 release

--- Sample/app/src/main/java/com/phunware/core/sample/SecondActivity.java
+++ Sample/app/src/main/java/com/phunware/core/sample/SecondActivity.java
@@ -5,32 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
-import com.phunware.core.PwCoreSession;
-
 public class SecondActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-		/*
-		 * Start Core Session
-		 */
-        PwCoreSession.getInstance().activityStartSession(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-		/*
-		 * Stop Core Session
-		 */
-        PwCoreSession.getInstance().activityStopSession(this);
     }
 
     @Override
