e939d6e6e82e170d088af300234527d5fd4b8782
Anu Vakkachen <avakkachen@phunware.com>
2020-04-27 16:36:35 -0500

    Preparing for release 3.5.2

--- Sample/app/src/main/java/com/phunware/core/sample/App.java
+++ Sample/app/src/main/java/com/phunware/core/sample/App.java
@@ -11,6 +11,5 @@ public class App extends Application {
         super.onCreate();
 
         PwLog.setShowLog(true);
-        this.registerActivityLifecycleCallbacks(new PwActivityLifecycleCallback());
     }
 }
--- Sample/app/src/main/java/com/phunware/core/sample/CoreDemo.java
+++ Sample/app/src/main/java/com/phunware/core/sample/CoreDemo.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import com.phunware.core.analytics.PwAnalyticsModule;
+
 public class CoreDemo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PwAnalyticsModule.enableAutomaticScreenViewEvents(true);
+        // screen views will be tracked automatically for all activities and fragments loaded after this
+        PwAnalyticsModule.registerScreenViews(this);
 
         findViewById(R.id.next).setOnClickListener(new OnClickListener() {
             @Override
